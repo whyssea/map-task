4bb95737b46df09bac5dbc4f8b57b49eb8bd1e73
whyssea <robot11148@gmail.com>
2024-11-19 13:45:56 +0700

    feat: add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import viteTsPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react(), viteTsPaths()],
-})
+  base: '/map-task/',
+}))
